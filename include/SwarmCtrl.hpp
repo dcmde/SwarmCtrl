59193d1519da193a76aa91b189c02f549bf1c55e
dcmde <43622430+dcmde@users.noreply.github.com>
2021-05-31 12:18:04 +0200

    WIP

--- include/SwarmCtrl.hpp
+++ include/SwarmCtrl.hpp
@@ -1,30 +1,68 @@
 #ifndef SWARMCTRL_SWARMCTRL_HPP
 #define SWARMCTRL_SWARMCTRL_HPP
 
+#include <vector>
+
+std::vector<double> operator+(const std::vector<double> &v1, const std::vector<double> &v2) {
+    std::vector<double> temp;
+    if (v1.size() != v2.size()) {
+        return temp;
+    }
+    for (int i = 0; i < v1.size(); ++i) {
+        temp.push_back(v1[i] + v2[i]);
+    }
+    return temp;
+}
+
+std::vector<double> operator+=(std::vector<double> &v1, const std::vector<double> &v2) {
+    std::vector<double> temp;
+    if (v1.size() != v2.size()) {
+        return temp;
+    }
+    for (int i = 0; i < v1.size(); ++i) {
+        temp.push_back(v1[i] + v2[i]);
+    }
+    v1 = temp;
+    return temp;
+}
+
 class SwarmCtrl {
 public:
-    void setBorders(std::vector<double>);
+    void setSampleTime(double Te);
 
-    void setRepParam(double);
+    void setBorders(const std::vector<double> &borders);
 
-    void setAttractParam(double);
+    void setRepParam(double repulsion);
+
+    void setAttractParam(double attract);
 
     void setMaxIteration(int maxIter);
 
-    std::vector<double> getOptimalPosition(std::vector <std::vector<double>> initialPositionSwarm,
-                                           std::vector<double> yourPosition);
+    std::vector<double> getOptimalPosition(const std::vector<std::vector<double>> &initialPositionSwarm,
+                                           const std::vector<double> &yourPosition);
 
-    std::vector<double> getLocalGradientDirection(std::vector <std::vector<double>> currentPosition,
-                                                  std::vector<double> yourPosition,
-                                                  bool= true);
+    std::vector<double> getLocalGradientDirection(std::vector<std::vector<double>> currentPosition,
+                                                  std::vector<double> optimalPosition,
+                                                  std::vector<double> yourPosition);
 
 protected:
 
-    inline std::vector<double> attractive(std::vector<double>);
+    int maxIter_{};
+    double rep_{}, atr_{}, Te_{};
+    std::vector<double> borders_{};
 
-    inline std::vector<double> border(std::vector<double>);
+    inline std::vector<double> sys(const std::vector<double> &X, const std::vector<double> &U);
 
-    inline std::vector<double> repulsive(std::vector<double>);
+    inline std::vector<double> attractive(std::vector<double> posOptimal,
+                                          std::vector<double> currentPos);
+
+    inline std::vector<double> border(std::vector<double> currentPos);
+
+    inline std::vector<double> repulsive(std::vector<double> otherDrones,
+                                         std::vector<double> currentPos);
+
+    inline std::vector<double> repulsive(std::vector<std::vector<double>> otherDrones,
+                                         std::vector<double> currentPos);
 };
 
 #endif //SWARMCTRL_SWARMCTRL_HPP
--- src/SwarmCtrl.cpp
+++ src/SwarmCtrl.cpp
@@ -1,2 +1,81 @@
 
 
+#include <SwarmCtrl.hpp>
+
+void SwarmCtrl::setBorders(const std::vector<double> &borders) {
+    borders_ = borders;
+}
+
+void SwarmCtrl::setRepParam(const double repulsion) {
+    rep_ = repulsion;
+}
+
+void SwarmCtrl::setAttractParam(const double attract) {
+    atr_ = attract;
+}
+
+void SwarmCtrl::setMaxIteration(const int maxIter) {
+    maxIter_ = maxIter;
+}
+
+std::vector<double> SwarmCtrl::getOptimalPosition(const std::vector<std::vector<double>> &initialPositionSwarm,
+                                                  const std::vector<double> &yourPosition) {
+    // Number of drones.
+    unsigned int n = initialPositionSwarm.size() + 1;
+    std::vector<double> U(2, 0), temp(4, 0);
+    std::vector<std::vector<double>> swarm_coords(n);
+    // Browse sample time
+    for (int i = 0; i < maxIter_ - 1; ++i) {
+        // Browse each drone in the swarm
+        for (int j = 0; j < n; ++j) {
+            // Compute drone repulsion
+            for (int k = 0; k < n; ++k) {
+                U += repulsive(swarm_coords[j], swarm_coords[k]);
+            }
+            // Border computation
+            U += border(swarm_coords[j]);
+
+            // System dynamic
+            temp[0] = swarm_coords[i][0] + U[0]; //Vx
+            temp[1] = swarm_coords[i][1] + U[1]; //Vy
+            temp[2] = swarm_coords[i][2] + swarm_coords[i][0] * Te_; //X
+            temp[3] = swarm_coords[i][3] + swarm_coords[i][1] * Te_; //Y
+            swarm_coords[i] = temp;
+        }
+    }
+    return std::vector<double>();
+}
+
+std::vector<double> SwarmCtrl::getLocalGradientDirection(std::vector<std::vector<double>> currentPosition,
+                                                         std::vector<double> optimalPosition,
+                                                         std::vector<double> yourPosition) {
+    std::vector<double> U;
+    U = repulsive(currentPosition, yourPosition);
+    U += attractive(optimalPosition, yourPosition);
+    return U;
+}
+
+std::vector<double> SwarmCtrl::attractive(std::vector<double> posOptimal, std::vector<double> currentPos) {
+    return std::vector<double>();
+}
+
+std::vector<double> SwarmCtrl::border(std::vector<double> currentPos) {
+    return std::vector<double>();
+}
+
+std::vector<double> SwarmCtrl::repulsive(std::vector<double> otherDrones, std::vector<double> currentPos) {
+    return std::vector<double>();
+}
+
+std::vector<double> SwarmCtrl::repulsive(std::vector<std::vector<double>> otherDrones, std::vector<double> currentPos) {
+    std::vector<double> temp;
+    for (const auto &it : otherDrones) {
+        temp += repulsive(it, currentPos);
+    }
+    return temp;
+}
+
+void SwarmCtrl::setSampleTime(double Te) {
+    Te_ = Te;
+}
+
